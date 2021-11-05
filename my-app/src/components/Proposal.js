68dd2b2d0618006220cff3f91b0eb00ea4f885b9
Mina Soha <87757115+minasoha@users.noreply.github.com>
2021-11-05 04:35:24 -0400

    added data

--- my-app/src/components/Proposal.js
+++ my-app/src/components/Proposal.js
@@ -4,7 +4,7 @@ export default function Proposal() {
   return (
     <div>
       <h3>Proposal Pictures</h3>
-      <img src="https://cdn.discordapp.com/attachments/417484256328155144/872346920763400252/IMG_3542.jpg" width="200" height="200"/>
+      <img src="https://cdn.discordapp.com/attachments/417484256328155144/872346920763400252/IMG_3542.jpg" width="450" height="450"/>
     </div>
   )
 }
