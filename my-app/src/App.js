83fa8cd754ef7d3276bcedef3b767b9d941d4cc6
Mina Soha <87757115+minasoha@users.noreply.github.com>
2021-11-05 01:54:20 -0400

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Proposal from './components/Proposal';
+import Wedding from './components/Wedding';
+function App() {
+  return (
+    <div className="App">
+      <h1>Noura & Mario</h1>
+      <Proposal/>
+      <Wedding/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Proposal.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Proposal() {
+  return (
+    <div>
+      <h3>Proposal Pictures</h3>
+      <img src="https://cdn.discordapp.com/attachments/417484256328155144/872346920763400252/IMG_3542.jpg" width="200" height="200"/>
+    </div>
+  )
+}
