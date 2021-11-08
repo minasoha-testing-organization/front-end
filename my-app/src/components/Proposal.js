f3ce759009d8542fe81200dae1a5fa1f3cf04a1f
Mina Soha <87757115+minasoha@users.noreply.github.com>
2021-11-08 01:14:35 -0500

    main

--- my-app/src/components/Proposal.js
+++ my-app/src/components/Proposal.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useCallback, useState } from 'react'
+import { Controlled as ControlledZoom } from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 import beforeWedding from "./../data";
 export default function Proposal() {
+  const [isZoomed, setIsZoomed] = useState(false)
+  const handleImgLoad = useCallback(() => {
+    setIsZoomed(true)
+  }, [])
+
+  const handleZoomChange = useCallback(shouldZoom => {
+    setIsZoomed(shouldZoom)
+  }, [])
   return (
     <div>
       <h3>Proposal Pictures</h3>
-      {beforeWedding.map((img) => {
-        console.log(img);
-        return <img src={img.img} width="500" height="500" />;
-      })}
+      <ControlledZoom isZoomed={isZoomed} onZoomChange={handleZoomChange}>
+        {beforeWedding.map((img) => {
+          console.log(img);
+          return <img src={img.img} width="500" onLoad={handleImgLoad} />;
+        })}
+      </ControlledZoom>
     </div>
   );
 }
