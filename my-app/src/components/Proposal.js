9b2f9bf250a3d837527351416a58464f0aadfe44
minasoha <minasoha@yahoo.com>
2021-11-23 01:36:04 -0500

    got the zoom to work properlly

--- my-app/src/components/Proposal.js
+++ my-app/src/components/Proposal.js
@@ -1,31 +1,71 @@
-import React, { useCallback, useState } from 'react'
-import { Controlled as ControlledZoom } from 'react-medium-image-zoom'
-import 'react-medium-image-zoom/dist/styles.css'
+import React, { useCallback, useState } from "react";
+import { Controlled as ControlledZoom } from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 import beforeWedding from "./../data";
-import {Carousel} from 'react-responsive-carousel'
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Zoom from "react-medium-image-zoom";
 export default function Proposal() {
-  const [isZoomed, setIsZoomed] = useState(false)
+  const [isZoomed, setIsZoomed] = useState(false);
   const handleImgLoad = useCallback(() => {
-    setIsZoomed(true)
-  }, [])
+    setIsZoomed(true);
+  }, []);
 
-  const handleZoomChange = useCallback(shouldZoom => {
-    setIsZoomed(shouldZoom)
-  }, [])
+  const handleZoomChange = useCallback((shouldZoom) => {
+    setIsZoomed(shouldZoom);
+  }, []);
+  const handleOnClick = (e) => {};
   return (
     <div>
       <h3>Proposal Pictures</h3>
-      <Carousel className="pictures" autoPlay={true} centerSlidePercentage="100" dynamicHeight={true} infiniteLoop={true}>
-      {/* <ControlledZoom isZoomed={isZoomed} onZoomChange={handleZoomChange}> */}
-        {beforeWedding.map((img) => {
-          console.log(img);
-          return <div className="proposalPictures"><img className="pictures" src={img.img} width="500"  /></div>;
-        })}
+      <section className="container">
+        {/* <ControlledZoom isZoomed={isZoomed} onZoomChange={handleZoomChange}> */}
 
-      {/* </ControlledZoom> */}
-      </Carousel>
-         </div>
-         
+        <div className="pic">
+          {beforeWedding.map((img) => {
+            console.log(img);
+            return (
+              <div className="pic">
+                <Zoom>
+                  <img
+                    alt="Pictures of Noura's and Mario's proposal"
+                    onLoad={handleImgLoad}
+                    className="pictures"
+                    src={img.img}
+                    width="250"
+                    height="250"
+                  />
+                </Zoom>
+              </div>
+            );
+          })}
+        </div>
+
+        {/* </ControlledZoom> */}
+      </section>
+
+      {/* <Carousel
+        className="pictures"
+        autoPlay={true}
+        centerSlidePercentage="50"
+        dynamicHeight={true}
+        infiniteLoop={true}
+      >
+        <ControlledZoom isZoomed={isZoomed} onZoomChange={handleZoomChange}>
+          {beforeWedding.map((img) => {
+            console.log(img);
+            return (
+              <div className="proposalPictures">
+                <img
+                  onLoad={handleImgLoad}
+                  className="pictures"
+                  src={img.img}
+                />
+              </div>
+            );
+          })}
+        </ControlledZoom>
+      </Carousel> */}
+    </div>
   );
 }
