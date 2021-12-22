e15c65a108c288925096bd17d73e519e3add9862
Mina <minasoha@yahoo.com>
2021-12-21 22:18:43 -0500

    dasoih

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,14 @@
-
-import './App.css';
-import Proposal from './components/Proposal';
-import Wedding from './components/Wedding';
+import "./App.css";
+import MasonryImageList from "./components/Proposal";
+import Wedding from "./components/Wedding";
 function App() {
-  return (
-    <div className="App">
-      <h1>Noura & Mario</h1>
-      <Proposal/>
-      <Wedding/>
-
-    </div>
-  );
+ return (
+  <div className="App">
+   <h1>Noura & Mario</h1>
+   <MasonryImageList />
+   <Wedding />
+  </div>
+ );
 }
 
 export default App;
