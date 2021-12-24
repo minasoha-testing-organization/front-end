e7cd3649fd3d431a1b38d6947f065f81969df182
Mina <minasoha@yahoo.com>
2021-12-23 20:55:56 -0500

    moved the files around

---
+++ src/components/Wedding.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Wedding() {
+  return (
+    <div>
+      <h3>Wedding Pictures</h3>
+      <img src="https://cdn.discordapp.com/attachments/417484256328155144/866919788289982504/image0.jpg" width="200" height="200" />
+    </div>
+  )
+}
